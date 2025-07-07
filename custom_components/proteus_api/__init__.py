73bedbe5782ea09344aa358cdbc86c5a17fbf638
Radek Buš <94534956+Busak007@users.noreply.github.com>
2025-07-07 08:17:00 +0200

    Update __init__.py

--- custom_components/proteus_api/__init__.py
+++ custom_components/proteus_api/__init__.py
@@ -1,1 +1,24 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "inverter_id": entry.data["inverter_id"],
+        "session_cookie": entry.data["session_cookie"],
+    }
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
