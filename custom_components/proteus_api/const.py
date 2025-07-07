ed7f534a0506dd22e5f4869141e8282cde9cf869
Radek Buš <94534956+Busak007@users.noreply.github.com>
2025-07-07 09:19:51 +0200

    Delete custom_components/proteus_api directory

--- custom_components/proteus_api/const.py
+++
@@ -1,7 +0,0 @@
-DOMAIN = "proteus"
-SCAN_INTERVAL = 30
-
-CONF_INVERTER_ID = "inverter_id"
-CONF_SESSION_COOKIE = "session_cookie"
-
-API_URL = "https://proteus.deltagreen.cz/api/trpc/inverters.detail,inverters.flexibilityRewardsSummary,inverters.controls.state,commands.current,inverters.currentStep"
