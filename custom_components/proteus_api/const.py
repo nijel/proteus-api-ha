f024cb5e50b9f530c38fc7f15d073e2d1a6d0bfb
Radek Buš <94534956+Busak007@users.noreply.github.com>
2025-07-07 08:26:43 +0200

    Update const.py

--- custom_components/proteus_api/const.py
+++ custom_components/proteus_api/const.py
@@ -1,1 +1,7 @@
-DOMAIN = "proteus_api"
+DOMAIN = "proteus"
+SCAN_INTERVAL = 30
+
+CONF_INVERTER_ID = "inverter_id"
+CONF_SESSION_COOKIE = "session_cookie"
+
+API_URL = "https://proteus.deltagreen.cz/api/trpc/inverters.detail,inverters.flexibilityRewardsSummary,inverters.controls.state,commands.current,inverters.currentStep"
